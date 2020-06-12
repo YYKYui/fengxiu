9e3985da7f450b53123f487bbe24e4fbcf347a89
YYKYui <981291596@qq.com>
2020-06-12 16:37:50 +0800

    YYKYui

--- index_moudle/js/index_cortroller.js
+++ index_moudle/js/index_cortroller.js
@@ -6,13 +6,13 @@ $(function(){
 	
 	$(".container_cortroller_jiben_8").click(function(){
 		loading_dst.removeClass("none");
-		console.log(loading_dst)
-		var mobilerestart = window.Android.intostart();
-		if(mobilerestart == "true"){
-			loading_dst.addClass("none");
-		}else{
-			$(".loading_dst_tishi").text("读取花样列表失败，请退出重试")
-		}
+		// console.log(loading_dst)
+		// var mobilerestart = window.Android.intostart();
+		// if(mobilerestart == "true"){
+		// 	loading_dst.addClass("none");
+		// }else{
+		// 	$(".loading_dst_tishi").text("读取花样列表失败，请退出重试")
+		// }
 		
 		// window.Android.Wenjian();
 		// var timerPnghost = setInterval(function(){
