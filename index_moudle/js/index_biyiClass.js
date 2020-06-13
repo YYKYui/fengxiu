214b13dfd7aff4ba187a9a374a4b6a8850ae7887
YYKYui <981291596@qq.com>
2020-06-13 16:12:04 +0800

    YYKYui

--- index_moudle/js/index_biyiClass.js
+++ index_moudle/js/index_biyiClass.js
@@ -11,26 +11,68 @@ $(function() {
 			console.log("111111")
 		}
 	});
-	
+
 	$('.pagination li').click(function() {
+		$(".allin_fenlei").addClass("none");
 		var i = $(this).index(); //下标第一种写法
 		//var i = $('.pagination').index(this);//下标第二种写法
 		$(this).addClass('selected').siblings().removeClass('selected');
 		$('#contents li').eq(i).show().siblings().hide();
+		$(this).text($(this).attr("data-title"))
 		var li_dataDis = $(this).attr("data-dis")
-		console.log("li_dataDis:"+li_dataDis)
-		$(".buyi_more_show").each(function(){
+		$(".buyi_more_show").each(function() {
 			var data_hotvalue = $(this).attr("data-hot");
-			
+			var data_allin = $(this).attr("data-ali");
 			var data_newvalue = $(this).attr("data-new");
-			
-			if(li_dataDis == data_hotvalue){
-				$(this).hide();
-			}else if(li_dataDis == data_newvalue){
-				$(this).hide();
-			}else{
+
+			if (li_dataDis == data_hotvalue) {
 				$(this).show();
+			} else if (li_dataDis == data_newvalue) {
+				$(this).show();
+			} else if (li_dataDis == data_allin) {
+				$(this).show();
+			} else {
+				$(this).hide();
 			}
 		})
 	});
+
+	$(".allin_show_fenlei").click(function() {
+		if($(".allin_fenlei").css("display")=="none"){
+			$(".allin_fenlei").removeClass("none");
+		}else{
+			$(".allin_fenlei").addClass("none")
+		}
+	});
+	
+	$(".allin_fenlei_div").click(function(){
+		var data_miaoshu = $(this).attr("data-miaoshu");
+		$(".allin_fenlei").addClass("none");
+		$(".pagination li").removeClass('selected');
+		$(".pagination li").eq(0).addClass("selected")
+		$(".buyi_more_show").each(function() {
+			var data_miaoshuvalue = $(this).attr("data-fenlei");
+		
+			if (data_miaoshu == data_miaoshuvalue) {
+				$(this).show();
+			}else {
+				$(this).hide();
+			}
+			
+		})
+		$(".fenlei_title").text($(this).text())
+		
+		
+	})
+	
+	$(".lode_more").click(function(){
+		$(".buyi_Class_index").addClass("none")
+		$(".more_neirong").removeClass("none")
+	})
+	$(".buyi_getmore").click(function(){
+		$(".buyi_Class_index").addClass("none")
+		$(".more_neirong").removeClass("none")
+	})
+
+
 })
