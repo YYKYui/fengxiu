b4522dd25b2c526c19c13f386a791ddc5da96e98
YYKYui <981291596@qq.com>
2020-06-17 18:28:18 +0800

    YYKYui

--- index_moudle/js/index_cortroller.js
+++ index_moudle/js/index_cortroller.js
@@ -3,15 +3,19 @@ $(function() {
 	
 	var loading_dst = $(".loading_dst");
 	var timerPnghost;
-	var dst_zhenshu = 0;
+	var huoqutimeer;
+	var dst_zhenshu;
+	var dst_zongzhenshu;
 	$(".container_cortroller_jiben_1").click(function() {
-		
-		if(dst_zhenshu==0){
-			
-			var dst_zongzhenshu = window.Android.zhenshu();
+		clearInterval(huoqutimeer);
+		if(dst_zhenshu==undefined){
+			dst_zhenshu=0;
+			dst_zongzhenshu = window.Android.zhenshu();
 			console.log(dst_zongzhenshu)
 			timerPnghost = setInterval(function() {
 				if ((dst_zhenshu < dst_zongzhenshu) && (dst_zongzhenshu - dst_zhenshu >8)) {
+					$(".container_cortroller_message_zhenshu_2").val(dst_zongzhenshu);
+					$(".container_cortroller_message_zhenshu_math_1").val(dst_zhenshu);
 					dst_zhenshu = dst_zhenshu + 8;
 					window.Android.Imagepng(dst_zhenshu); //0 为整图 数值为针数
 					var imagedata = window.Android.Imagepng(dst_zhenshu);
@@ -20,11 +24,12 @@ $(function() {
 					} else {
 						var draw_table = document.getElementById("draw_table");
 						$("#draw_table").attr("src", "data:image/png;base64," + imagedata);
-						$(".container_cortroller_message_zhenshu_2").text(dst_zongzhenshu);
-						$(".container_cortroller_message_zhenshu_math_1").text(dst_zhenshu);
+
 					}
 				} else {
 					dst_zhenshu = dst_zongzhenshu;
+					$(".container_cortroller_message_zhenshu_2").val(dst_zongzhenshu);
+					$(".container_cortroller_message_zhenshu_math_1").val(dst_zhenshu);
 					window.Android.Imagepng(dst_zhenshu); //0 为整图 数值为针数
 					var imagedata = window.Android.Imagepng(dst_zhenshu);
 					if (imagedata == undefined) {
@@ -32,8 +37,7 @@ $(function() {
 					} else {
 						var draw_table = document.getElementById("draw_table");
 						$("#draw_table").attr("src", "data:image/png;base64," + imagedata);
-						$(".container_cortroller_message_zhenshu_2").text(dst_zongzhenshu);
-						$(".container_cortroller_message_zhenshu_math_1").text(dst_zhenshu);
+
 					}
 				}
 			}, 200);
@@ -44,30 +48,45 @@ $(function() {
 
 	})
 	$(".container_cortroller_jiben_8").click(function() {
-		loading_dst.removeClass("none");
-		var filenum = window.Android.filenum();
-		filenum = filenum * 0.2;
-		console.log(filenum)
-		var get_listTimeout = setTimeout(function() {
-			var mobilerestart = window.Android.intostart();
-			if (mobilerestart == "true") {
-				loading_dst.addClass("none");
-				window.Android.Wenjian();
-			} else {
-				$(".loading_dst_tishi").text("读取花样列表失败，请退出重试")
-			}
-		}, filenum);
-
-
-
-		window.Android.Imagepng(0); //0 为整图 数值为针数
-		var imagedata = window.Android.Imagepng(0);
-		if (imagedata == undefined) {
-			$(".dst_zhanshi").text("传输失败请重新上传图片")
-		} else {
-			var draw_table = document.getElementById("draw_table");
-			$("#draw_table").attr("src", "data:image/png;base64," + imagedata)
+		if(dst_zhenshu == dst_zongzhenshu && dst_zhenshu == 0){
+			loading_dst.removeClass("none");
+			var filenum = window.Android.filenum();
+			filenum = filenum * 0.2;
+			console.log(filenum)
+			var get_listTimeout = setTimeout(function() {
+				var mobilerestart = window.Android.intostart();
+				if (mobilerestart == "true") {
+					loading_dst.addClass("none");
+					window.Android.Wenjian();
+				} else {
+					$(".loading_dst_tishi").text("读取花样列表失败，请退出重试")
+				}
+			}, filenum);
+			
+			clearTimeout(get_listTimeout)
+			huoqutimeer=setInterval(function(){
+				clearInterval(timerPnghost)
+				var dst_zongzhenshu = window.Android.zhenshu();
+				$(".container_cortroller_message_zhenshu_2").val(dst_zongzhenshu);
+				$(".container_cortroller_message_zhenshu_math_1").val(dst_zhenshu);
+				window.Android.Imagepng(0); //0 为整图 数值为针数
+				var imagedata = window.Android.Imagepng(0);
+				if (imagedata == undefined) {
+					$(".dst_zhanshi").text("传输失败请重新上传图片")
+				} else {
+					var draw_table = document.getElementById("draw_table");
+					$("#draw_table").attr("src", "data:image/png;base64," + imagedata)
+				}
+			},1111)
+		}else{
+			$(".error_message").removeClass("none");
+			$(".error_message").text("工作中无法上传文件")
+			var qingchu = window.setTimeout(function(){
+				$(".error_message").addClass("none");
+			},2000)
+			clearTimeout(qingchu);
 		}
+	
 	})
 
 
@@ -97,7 +116,7 @@ $(function() {
 		ctx.stroke();
 	}
 	var wangge_star_off = true;
-	$(".container_cortroller_jiben_6 button_click").click(function() {
+	$(".container_cortroller_jiben_6").click(function() {
 		if (wangge_star_off) {
 			var dst_background = myCanvas.toDataURL('image/png', 1);
 			$(".dst_zhanshi").css("background", "url(" + dst_background + ") repeat");
