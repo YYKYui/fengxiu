e8ccfa34d34476acf1143be39f8a23c79587e010
YYKYui <981291596@qq.com>
2020-06-19 15:44:25 +0800

    YYKYui

--- my_info/js/info_xiugai.js
+++ my_info/js/info_xiugai.js
@@ -3,11 +3,11 @@ $(function(){
 	$(".change_touxiang").click(function(){
 		window.Android.photolist();
 		timericonstruc=setInterval(function(){
-			var isok_img = window.Android.gettrue();
+			var isok_img = window.Android.getlook();
 			if(isok_img==true){
 				window.Android.phototrue();
 				var imgdata = window.Android.phototrue();
-				$(".img_icon").attr("src","data:image/png;base64," + imagedata);
+				$(".img_icon").attr("src","data:image/png;base64," + imgdata);
 				clearInterval(timericonstruc);
 			}
 		},200)
